9f321ed174dc61f99bb6386b14b70107f76873d2
Rachel Peterson <rachelp5446@gmail.com>
2020-10-01 21:39:45 -0500

    Update existing files

--- playground.py
+++ playground.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/play')
+def level1():
+    return render_template('index.html')
+
+@app.route('/play/<int:num>')
+def play(num):
+    return render_template('index1.html', num=num)
+
+@app.route('/play/<int:num>/<color>')
+def color(num,color):
+    return render_template('index2.html', num=num, color=color)
+
+if __name__=="__main__":
+    app.run(debug=True)
